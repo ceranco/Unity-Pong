56df19278d88449b378ff9c1cf21ee8bd2834a5b
ceranco <ceranco@gmail.com>
2018-03-01 20:11:33 +0200

    Created CountdownTimer prefab
    Created CountDown timer, which counts down using a coroutine, and raises an OnCountdownFinished event. Also organized the assets into sub-folders.

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    #region Private Members
+
+    /* The text of the timer */
+    Text timerText;
+
+    /* The current countdown coroutine */
+    Coroutine currentCountdown;
+
+
+    #endregion
+
+    #region Public Properties
+
+
+    /// <summary>
+    /// The font size of the timer.
+    /// </summary>
+    public int FontSize
+    {
+        get { return timerText.fontSize; }
+        set { timerText.fontSize = value; }
+    }
+
+
+    /// <summary>
+    /// Whether the timer is active.
+    /// </summary>
+    public bool IsActive
+    {
+        get { return gameObject.activeSelf; }
+        private set { gameObject.SetActive(value); }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Initializing the GameObject.
+    /// </summary>
+    private void Start()
+    {
+        // getting the text componet
+        timerText = GetComponent<Text>();
+       
+        // starting the timer is not active
+        IsActive = false;
+    }
+
+    /// <summary>
+    /// Coroutine for the countdown.
+    /// </summary>
+    /// <param name="startNumber">The number to count down from.</param>
+    /// <param name="delay">The delay between each count in seconds.</param>
+    /// <returns></returns>
+    private IEnumerator CountdownRoutine(uint startNumber, float delay)
+    {
+        for (int i = (int)startNumber; i > 0; i--)
+        {
+            timerText.text = i.ToString();
+            yield return new WaitForSeconds(delay);
+        }
+        // raising the CountdownFinshed event
+        OnCountdownFinished(new EventArgs());
+        IsActive = false;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Starts the count down coroutine.
+    /// </summary>
+    /// <param name="startNumber">The number to count down from.</param>
+    /// <param name="delay">The delay between each count in seconds.</param>
+    public void StartCountdown(uint startNumber, float delay)
+    {
+        if (!IsActive)
+        {
+            IsActive = true;
+            currentCountdown = StartCoroutine(CountdownRoutine(startNumber, delay));
+        }
+    }
+
+    public void StopCountdown()
+    {
+        if (IsActive)
+        {
+            StopCoroutine(currentCountdown);
+        }
+        IsActive = false;
+    }
+
+    #endregion
+
+    #region Event Members
+
+    public event EventHandler CountdownFinished;
+
+    protected virtual void OnCountdownFinished(EventArgs e)
+    {
+        EventHandler handler = CountdownFinished;
+        handler?.Invoke(this, e);
+    }
+
+    #endregion
+}
